3f8aeb2868a4e720900aef875ee29c3c595835ac
Jesus Rodriguez <jesusrodriguezgonzalezgr@gmail.com>
2022-06-30 20:23:01 +0200

    Connect component Product with api

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import './App.css';
 import { ContainerHome } from './components/ContainerHome';
 import { Layout } from './components/core/Layout';
 import { DetailsProducts } from './components/products/DetailsProduct';
-import { getAllProducts } from '../src/services/api';
 
 function App() {
-  getAllProducts().then((data) => console.log(data.data));
   return (
     <div className="App">
       <Layout>
